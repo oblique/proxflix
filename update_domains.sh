fdf621a29fc3e9629abb453ebc4ec3517d00fc4a
oblique <psyberbits@gmail.com>
2017-06-29 18:25:24 +0300

    Add cachefly.net
    To check your speed run:
      wget https://cachefly.cachefly.net/100mb.test

--- update_domains.sh
+++ update_domains.sh
@@ -2,3 +2,6 @@
 
 curl https://raw.githubusercontent.com/ab77/netflix-proxy/master/data/conf/zones.override.template | \
     awk '/^zone\s/ { if (match($2, /^"(.+)\."$/, m) != 0) print m[1]; }' > domains
+
+# to check your speed run: wget https://cachefly.cachefly.net/100mb.test
+echo cachefly.net >> domains
